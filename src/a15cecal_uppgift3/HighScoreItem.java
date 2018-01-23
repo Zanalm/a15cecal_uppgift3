64d7d028a196a407581740c20f93c2306a66bbdf
Zander <31848787+Zanalm@users.noreply.github.com>
2018-01-23 11:25:54 +0100

    Initial commit

---
+++ src/a15cecal_uppgift3/HighScoreItem.java
@@ -0,0 +1,5 @@
+package a15cecal_uppgift3;
+
+public class HighScoreItem {
+
+}
---
+++ src/a15cecal_uppgift3/HighScoreList.java
@@ -0,0 +1,10 @@
+package a15cecal_uppgift3;
+
+public class HighScoreList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
