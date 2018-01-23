d1d5f173d26315553633843ee2747b5dc47d19cf
Zander <31848787+Zanalm@users.noreply.github.com>
2018-01-23 15:51:03 +0100

    saves some text to the drive

--- src/a15cecal_uppgift3/HighScoreItem.java
+++ src/a15cecal_uppgift3/HighScoreItem.java
@@ -1,5 +1,5 @@
 package a15cecal_uppgift3;
 
 public class HighScoreItem {
-
+	// one function for score, one for name
 }
--- src/a15cecal_uppgift3/HighScoreList.java
+++ src/a15cecal_uppgift3/HighScoreList.java
@@ -1,9 +1,39 @@
 package a15cecal_uppgift3;
+import java.io.*;
 
 public class HighScoreList {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// The name of the file to open.
+        String fileName = "highscorelist.txt";
+
+        try {
+            // Assume default encoding.
+            FileWriter fileWriter =
+                new FileWriter(fileName);
+
+            // Always wrap FileWriter in BufferedWriter.
+            BufferedWriter bufferedWriter =
+                new BufferedWriter(fileWriter);
+
+            // Note that write() does not automatically
+            // append a newline character.
+            bufferedWriter.write("Hi");
+            bufferedWriter.write(" I'm dog.");
+            bufferedWriter.newLine();
+            bufferedWriter.write("I'll be your dog");
+            bufferedWriter.write(" for the evening.");
+
+            // Always close files.
+            bufferedWriter.close();
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error writing to file '"
+                + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
 
 	}
 
